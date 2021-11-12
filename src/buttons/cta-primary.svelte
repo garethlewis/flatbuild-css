2df87e0f8d40bb91a14a94f227ac69bcbf2debb9
Gareth Lewis <gareth_lewis2@mac.com>
2021-11-12 09:13:29 +0000

    Use plugin to order tailwind classes

--- src/buttons/cta-primary.svelte
+++ src/buttons/cta-primary.svelte
@@ -3,9 +3,9 @@
     import mediumright from '../icons/chevrons/mediumright.svg';
 </script>
 
-<div class="group w-full cursor-pointer">
-    <div class="sm:inline-block w-10/12 transition-all duration-200 group-hover:w-11/12">
-        <div class="flex justify-center bg-aldermore-yellow px-4 py-2">
+<div class="w-full cursor-pointer group">
+    <div class="w-10/12 transition-all duration-200 sm:inline-block group-hover:w-11/12">
+        <div class="flex justify-center px-4 py-2 bg-aldermore-yellow">
             <p>{text}</p>
             <span class="flex items-center">
                 <div class="flex justify-center w-5 h-5 transition-all duration-200 transform group-hover:translate-x-3">
